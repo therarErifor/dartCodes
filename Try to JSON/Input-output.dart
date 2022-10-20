d9db4054a405df7a251f0a3aa2c89c3b2cec30cf
therarErifor <46041526+therarErifor@users.noreply.github.com>
2022-10-20 22:22:06 +0500

    Add files via upload

--- Try to JSON/Input-output.dart
+++ Try to JSON/Input-output.dart
@@ -6,15 +6,40 @@ void main(){
   print("Введите данные об автомобиле");
 
   print("Марка автомобиля:");
+
   var brand = stdin.readLineSync()!;
+  while (brand == ""){
+    print("Пожалуйста, введите марку автомобиля!:");
+    brand = stdin.readLineSync()!;
+  };
+
   print("Модель автомобиля:");
   var model = stdin.readLineSync()!;
+  while (model == ""){
+    print("Пожалуйста, введите модель автомобиля!:");
+    model = stdin.readLineSync()!;
+  };
+
   print("Цвет автомобиля:");
   var color = stdin.readLineSync()!;
-  print("Госномер автомобиля");
+  while (color == ""){
+    print("Пожалуйста, введите цвет автомобиля!:");
+    color = stdin.readLineSync()!;
+  };
+
+  print("Госномер автомобиля:");
   var carNumber = stdin.readLineSync()!;
-  print("Имя владельца");
+  while (carNumber == ""){
+    print("Пожалуйста, введите госномер автомобиля!:");
+    carNumber = stdin.readLineSync()!;
+  };
+
+  print("Имя владельца автомобиля:");
   var carOwnerName = stdin.readLineSync()!;
+  while (carOwnerName == ""){
+    print("Пожалуйста, введите имя владельца автомобиля!:");
+    carOwnerName = stdin.readLineSync()!;
+  };
 
   Car tom = Car(brand: brand, model: model, color: color,
   carNumber: carNumber, carOwnerName: carOwnerName);
