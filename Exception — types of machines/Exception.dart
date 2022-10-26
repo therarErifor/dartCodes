677edb6e43ec1599ae297aad371a1ac2650bd190
therarErifor <therar.erifor@gmail.com>
2022-10-26 21:22:24 +0500

    exception handling
    downloaded and updated directories with tasks related to exception handling

--- Exception — types of machines/Exception.dart
+++ Exception — types of machines/Exception.dart
@@ -5,29 +5,41 @@ void main(){
   exception(path);
 }
 
-//Copy this path's:
-//correct.json
-//json_with_syntax_error.json
+//Copy this path's ==>  correct_bus.json
+//                      correct_passenger.json
+//                      correct_truck.json
+//                      json_with_syntax_error.json
+//                      non_correct_type.json
 
 enterThePath(){
   // this is where the JSON file is read and serialized
   print("Enter the path, please");
   var path = stdin.readLineSync()!;
 
-
-  while (path == '') {
+  while (path == "") {
     print("Enter the path, please");
     path = stdin.readLineSync()!;
   }
+
   return path;
 }
 exception (String path) async{
   var contents;
+
   try {       //exception operation
   contents = await File(path).readAsString();
-    final Map<String, dynamic> data = jsonDecode(contents);
-    var car = Car.fromJson(data);
+  final Map<String, dynamic> data = jsonDecode(contents);
+   var car = Car.fromJson(data);
 
+  switch (car.type)  {      // (car.type != "passenger") || b != "truck" || b != ("bus")
+    case "passenger":
+    case "truck":
+    case "bus":
+      print(car.type);
+      break;
+    default:
+      throw TypeMachineException();
+  }
   print(car.brand);
   print(car.model);
   print(car.color);
@@ -42,20 +54,28 @@ exception (String path) async{
     print("the file format is incorrect!");
     main();
   }
+  on TypeMachineException {
+    print("invalid machine type specified in the file");
+    main();
+  }
+}
+
+class TypeMachineException implements Exception{
 }
 
 class Car {
+  final String type;
   final String brand;
   final String model;
   final String color;
   final String carNumber;
   final String carOwnerName;
 
-  Car({required this.brand, required this.color, required this.model,
+  Car({required this.brand, required this.type, required this.color, required this.model,
     required this.carNumber, required this.carOwnerName});
 
   factory Car.fromJson(Map<String, dynamic> car) {
-    return Car(brand: car["brand"], color: car["color"],  model: car["model"],
+    return Car(type: car["type"], brand: car["brand"], color: car["color"],  model: car["model"],
         carNumber: car["carNumber"], carOwnerName: car["carOwnerName"]);
   }
 }
